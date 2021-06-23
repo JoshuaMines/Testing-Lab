a80ac12ecfd595d09ba49305f9108fb473c02167
Joshua Mines <minesjoshua@gmail.com>
2021-06-22 20:43:10 -0600

    final

--- function.js
+++ function.js
@@ -3,7 +3,7 @@ returnTwo: function() {
     return (2);
 },
 greeting: function(name) {
-    return ("Hello, name.")
+    return `Hello, ${name}`;
 },
 add: function (num1, num2) {
     return (num1 + num2)
--- functions.test.js
+++ functions.test.js
@@ -2,9 +2,25 @@ const { expect, test, describe} = require('@jest/globals');
 const {returnTwo, greeting, add} = require('./function.js');
 
 test('returnTwo should return the number 2', function(){
-    expect([2]).toEqual([2])
+    expect(returnTwo(2)).toEqual(2)
 });
 
-test('greeting should return a string', function(){
-    expect(greeting)
+describe ('this function should return a string with a name', function(){
+        test('name mike, should return Hello, mike', function(){
+        expect(greeting('mike')).toEqual('Hello, mike')
+    })
+        test('name sam, should return Hello, sam', function(){
+        expect(greeting('sam')).toEqual('Hello, sam')
+        })
+})
+
+
+describe('this function should return the sum of two numbers', function(){
+    test('1 and 2', function(){
+        expect(add(1, 2)).toEqual(3)
+    });
+    
+    test('5 and 9', function(){
+        expect(add(5, 9)).toEqual(14)
+    })
 })
